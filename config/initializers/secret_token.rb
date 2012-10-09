b7c53eaa3e4f29ef789693f3f0ec502f03ea0f4a
Quy Phan <quyp@qualcomm.com>
2012-10-08 21:21:46 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroundZero::Application.config.secret_token = 'c51a0236253cbaf9dfa85d01f95c6c199d0ec1717f46c6ad638cab91921205e898dc1346ad1f4b25220408a12d073e897861d9847e010664a309b50846c93722'
